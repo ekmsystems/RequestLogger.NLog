59bba3fbdf844874bbe1a6d0daeeddca23914ae3
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-21 11:03:09 +0100

    Added an example ASP.NET web project.

---
+++ src/Examples/AspNetExample/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using NLog;
+using RequestLogger.NLog;
+using RequestLogger.Web;
+
+namespace AspNetExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly RequestLoggerModule RequestLoggerModule = CreateRequestLoggerModule();
+
+        public override void Init()
+        {
+            base.Init();
+
+            RequestLoggerModule.Init(this);
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private static RequestLoggerModule CreateRequestLoggerModule()
+        {
+            var logger = LogManager.GetLogger("RequestLogger");
+            var requestLogger = new NLogLogger(logger);
+            return new RequestLoggerModule(requestLogger);
+        }
+    }
+}
