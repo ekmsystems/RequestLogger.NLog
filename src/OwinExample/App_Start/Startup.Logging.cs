740369ac01aeeb6f45031fada60742ad904924b0
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-04-21 11:08:31 +0100

    Added an Owin example.

---
+++ src/OwinExample/App_Start/Startup.Logging.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using Owin;
+using RequestLogger.NLog;
+using RequestLogger.Owin;
+
+namespace OwinExample
+{
+    public partial class Startup
+    {
+        private static void ConfigureLogging(IAppBuilder app)
+        {
+            var logger = LogManager.GetLogger("RequestLogger");
+            var requestLogger = new NLogLogger(logger);
+
+            app.UseRequestLoggerMiddleware(requestLogger);
+        }
+    }
+}
---
+++ src/OwinExample/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinExample.Startup))]
+namespace OwinExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureLogging(app);
+
+            app.Run(ctx =>
+            {
+                ctx.Response.ContentType = "text/plain";
+                return ctx.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
